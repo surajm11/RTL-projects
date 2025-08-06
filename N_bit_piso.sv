61cde9ac570c7d5bc53fb609917c512dd75d336e
surajm11 <112797182+surajm11@users.noreply.github.com>
2025-08-06 13:17:13 +0530

    Update N_bit_piso.sv

--- N_bit_piso.sv
+++ N_bit_piso.sv
@@ -11,28 +11,36 @@ module N_bit_piso
   
   reg [N-1:0] temp;
   reg [$clog2(N):0] count;
+  reg sending;
   
   always@(posedge clk or negedge rst_n) begin
     if(!rst_n) begin
       temp <= 'b0;
       count <= 'b0;
+      sending <= 'b0;
     end
     else if(load) begin
       temp <= din;
       count <= 'b0;
+      sending <= 'b1;
     end
-    else if(enable) begin // what if enable is high for all the clk cycles?
+    else if(enable && sending) begin // what if enable is high for all the clk cycles?
       dout <= temp[N-1];
       temp <= temp << 1;
-      if(count == N) begin
-        count <= 'b0; 
-        valid <= 'b0; 
+      valid <= 'b1;
+      if(count == N-1) begin
+        count <= 'b0;
+        sending <= 'b0;
       end
-      else begin
-        valid <= 'b1;
+      else begin      
         count <= count + 1'b1;
-      end        
-    end     
+        sending <= 'b1;
+      end
+    end
+    else begin
+      dout <= 'b0;
+      valid <= 'b0;
+    end  
   end
   
 endmodule
